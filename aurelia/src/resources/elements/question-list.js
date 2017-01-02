497c3b30c6fb3862f3c3f7fc5b7d27bbfb77af68
Aleksandar Preočanin <Aleksandar Preoanin>
2017-01-02 18:06:44 +0100

    Dodate promene u izgledu pitanja.

--- aurelia/src/resources/elements/question-list.js
+++ aurelia/src/resources/elements/question-list.js
@@ -6,11 +6,20 @@ export class QuestionList {
   @bindable value;
 
   constructor(httpClient) {
+    this.currentUser = { isDefined: false };
     this.httpClient = httpClient;
 
     this.httpClient.fetch('questions')
       .then(response => response.json())
       .then(questions => this.questions = questions);
+
+    this.httpClient.fetch('users/me')
+      .then(response => response.json())
+      .then(user => {
+        this.currentUser.id = user._id;
+        this.currentUser.name = user.name;
+        this.currentUser.isDefined = true;
+      });
   }
 
   valueChanged(newValue, oldValue) {
